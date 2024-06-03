d7223bd4b0a98e3ec2a8fb399cb5200c265ad4cb
wee-owl <kem-cv@yandex.ru>
2024-06-03 19:16:52 +0300

    feat: add database info

---
+++ src/utils/data.js
@@ -0,0 +1,235 @@
+const data = [
+  {
+    id: "avatar",
+    isNav: false,
+    photo: require("#assets/photo.png"),
+    name: "Елена Коробова",
+    position: "Frontend-разработчик"
+  },
+  {
+    id: "about",
+    isNav: true,
+    title: "о себе",
+    icon: "about",
+    content: {
+      text: "Работала инженером-разработчиком в компании оборонно-промышленного комплекса. Сменила сферу деятельности на программирование. Прошла профессиональную переподготовку по курсу «Frontend-разработчик с нуля» в Нетологии.\nУмею решать локальные задачи, которые хорошо декомпозированы и укладываются в принятые в проекте соглашения и архитектурные принципы. Хорошо знаю основной инструментарий – HTML, CSS, JavaScript. Уверенно владею React на уровне, который позволяет решать абсолютное большинство стандартных задач, связанных с интерфейсами.\nУмею ясно излагать свои мысли в письменной и устной форме и понимаю важность проактивности для командной работы. Я постоянно расширяю свои профессиональные компетенции в разработке и программировании."
+    }
+  },
+  {
+    id: "skills",
+    isNav: true,
+    title: "навыки",
+    icon: "skills",
+    content: [
+      {
+        title: "stack",
+        array: ["react", "react\u00A0router", "typescript", "javascript", "css", "html", "node.js", "jest", "markdown"]
+      },
+      {
+        title: "tools",
+        array: ["git", "github", "vs code", "figma", "photoshop"]
+      }
+    ]
+  },
+  {
+    id: "projects",
+    isNav: true,
+    title: "проекты",
+    icon: "projects",
+    content: [
+      {
+        title: "railway booking system",
+        cover: require("#assets/cover-railway.png"),
+        link: "https://wee-owl.github.io/fe-diploma/"
+      },
+      {
+        title: "chaos organizer",
+        cover: require("#assets/cover-organizer.png"),
+        link: "https://wee-owl.github.io/chaos-organizer/"
+      },
+      {
+        title: "like trello",
+        cover: require("#assets/cover-trello.png"),
+        link: "https://wee-owl.github.io/ahj_dnd_like_trello/"
+      },
+      {
+        title: "help desk",
+        cover: require("#assets/cover-helpdesk.png"),
+        link: "https://wee-owl.github.io/ahj_http_helpdesk/"
+      },
+      {
+        title: "goblin game",
+        cover: require("#assets/cover-goblin.png"),
+        link: "https://wee-owl.github.io/ahj_event_goblin-game/"
+      },
+      {
+        title: "tetris",
+        cover: require("#assets/cover-tetris.png"),
+        link: "https://wee-owl.github.io/Tetris/"
+      },
+      {
+        title: "your meal",
+        cover: require("#assets/cover-yourmeal.png"),
+        link: "https://wee-owl.github.io/your_meal/"
+      },
+      {
+        title: "songbird quiz",
+        cover: require("#assets/cover-songbird.png"),
+        link: "https://wee-owl.github.io/quiz/index.html"
+      },
+      {
+        title: "photo stream",
+        cover: require("#assets/cover-photostream.png"),
+        link: "https://photo-stream-wee-owl.vercel.app/"
+      },
+      {
+        title: "pomodoro",
+        cover: require("#assets/cover-pomodoro.png"),
+        link: "https://wee-owl.github.io/pomodoro/"
+      },
+      {
+        title: "online zoo",
+        cover: require("#assets/cover-zoo.png"),
+        link: "https://wee-owl.github.io/online-zoo/main.html"
+      },
+      {
+        title: "barbershop",
+        cover: require("#assets/cover-barbershop.png"),
+        link: "https://wee-owl.github.io/barbershop/"
+      },
+      {
+        title: "money exchange service",
+        cover: require("#assets/cover-moneyexchange.png"),
+        link: "https://wee-owl.github.io/Money_exchange_service/"
+      },
+      {
+        title: "shelter",
+        cover: require("#assets/cover-shelter.png"),
+        link: "https://wee-owl.github.io/shelter/shelter/main.html"
+      },
+      {
+        title: "noemi",
+        cover: require("#assets/cover-noemi.png"),
+        link: "https://wee-owl.github.io/Noemi/"
+      },
+      {
+        title: "travel portal",
+        cover: require("#assets/cover-travelportal.png"),
+        link: "https://wee-owl.github.io/TravelPortal/"
+      },
+    ]
+  },
+  {
+    id: "experience",
+    isNav: true,
+    title: "опыт",
+    icon: "experience",
+    content: [
+      {
+        position: "Frontend-разработчик",
+        period: "05.2022 - 05.2024",
+        company: "ООО «Нетология»",
+        title: "Учебные проекты",
+        text: [
+          "В рамках учебных проектов занималась разработкой клиентской части веб-приложений, создавала соответствующую макетам внешнюю оболочку сайта, настраивала рабочий функционал и связывала его с бэкендом"
+        ]
+      },
+      {
+        position: "Инженер-разработчик",
+        period: "02.2017 – 09.2021",
+        company: "АО «Концерн «Моринсис-Агат», г. Москва",
+        title: "Задачи/достижения",
+        text: [
+          "разработка технической документации и ведение проектов в государственной структуре оборонно-промышленного комплекса (разработала и ввела в эксплуатацию 48 документов по 6 проектам в соответствии с ГОСТ, РД и ЕСКД (ТЗ, ТУ, РЭ, ПС, УТП и т.д.)",
+          "комплексная работа с организациями-заказчиками и организациями-подрядчиками по разработке и корректировке ТЗ, анализу и планированию трудозатрат на выполнение работ",
+          "организация и проведение комплексных технических экспертиз КД и ТД в соответствии с ГОСТ и РВ (организовала и провела 12 технических экспертиз)",
+          "контроль исполнительных команд на объектах ведения разработок и эксплуатации систем и комплексов, своевременное выявление и устранение технических неисправностей"
+        ]
+      },
+      {
+        position: "Специалист по работе с образовательными учреждениями",
+        period: "07.2014 – 01.2017",
+        company: "АО «Концерн «Моринсис-Агат», г. Москва",
+        title: "Задачи/достижения",
+        text: [
+          "организация взаимовыгодного сотрудничества с учебными учреждениями СПО и ВПО для подготовки кандидатов технических специальностей в рамках обучения по целевой программе",
+          "организовала и заключила 10 договоров и соглашений о сотрудничестве с образовательными учреждениями (2014\u00A0г. – 1\u00A0договор, 2017\u00A0г. – 11\u00A0договоров)",
+          "увеличила количество абитуриентов, поступающих по целевой программе (2014\u00A0г. – 11\u00A0человек, 2017\u00A0г. – 43\u00A0человека)",
+          "расширила круг проводимых профориентационных мероприятий (2014\u00A0г. – 2\u00A0мероприятия в год, 2017\u00A0г. – в среднем до 40 мероприятий в год)"
+        ]
+      }
+    ]
+  },
+  {
+    id: "education",
+    isNav: true,
+    title: "образование",
+    icon: "education",
+    content: [
+      {
+        qualification: "Frontend-разработчик, профессиональная переподготовка",
+        year: "2024",
+        institution: "ООО «Нетология»",
+        city: "г.\u00A0Москва"
+      },
+      {
+        qualification: "Информационные системы и технологии, магистр",
+        year: "2016",
+        institution: "Российский технологический университет «МИРЭА»",
+        city: "г.\u00A0Москва"
+      },
+      {
+        qualification: "Управление и информатика в технических системах, инженер",
+        year: "2014",
+        institution: "Национальный исследовательский технологический университет «МИСиС»",
+        city: "г.\u00A0Москва"
+      }
+    ]
+  },
+  {
+    id: "language",
+    isNav: false,
+    title: "языки",
+    icon: "language",
+    content: [
+      {
+        language: "русский язык",
+        level: "родной"
+      },
+      {
+        language: "английский язык",
+        level: "А2 — B1"
+      }
+    ]
+  },
+  {
+    id: "contacts",
+    isNav: true,
+    title: "контакты",
+    icon: "contacts",
+    content: [
+      {
+        social: "github",
+        icon: "github",
+        link: "https://github.com/wee-owl/"
+      },
+      {
+        social: "discord",
+        icon: "discord",
+        link: "https://discordapp.com/users/@lienone"
+      },
+      {
+        social: "email",
+        icon: "email",
+        link: "mailto:kem-cv@yandex.ru"
+      },
+      {
+        social: "telegram",
+        icon: "telegram",
+        link: "https://t.me/emkorobova"
+      }
+    ]
+  }
+];
+
+export default data;
