210036c29fc67dfd4609720205a1fa130177eff6
wee-owl <kem-cv@yandex.ru>
2024-06-03 19:13:25 +0300

    feat: add start component

---
+++ src/views/StartPage/StartPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Main from "../Main/Main";
+import Header from "#components/Header/Header";
+import Footer from "#components/Footer/Footer";
+
+
+function StartPage() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default StartPage;
